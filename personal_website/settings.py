8340515707eb04003f990936b75f22657c63eca9
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-28 16:48:13 +0300

    Load environment variables and update ALLOWED_HOSTS in settings.py

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,10 @@ SECRET_KEY = "django-insecure-%@cq)nuqlbmu#nq^+op2g*x-7vmmyg_2yu&c^*a)tt4x+aw@ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", " ").split(" ") + [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
