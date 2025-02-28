9071f04562636cfd57fd3180b8302449fce45022
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-28 18:45:04 +0300

    Refactor deployment configuration to remove unused services and update environment variables for Django settings

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%@cq)nuqlbmu#nq^+op2g*x-7vmmyg_2yu&c^*a)tt4x+aw@ab"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
