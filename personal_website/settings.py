28d023f2b5c92c788edc7d7c189322636f615551
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-28 17:20:10 +0300

    Configure production settings for SQLite database and create data directory

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -82,12 +82,20 @@ WSGI_APPLICATION = "personal_website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get("BUILD_ENV") == "production":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "/app/data/db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
