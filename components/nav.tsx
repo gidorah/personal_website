208b29f3d72d8cba53d450ba259d549b5c1fd6bf
Onur Akyüz <onur_akyuz@icloud.com>
2025-03-12 19:49:22 +0300

    Comment out ThemeSwitcher component in navigation

--- components/nav.tsx
+++ components/nav.tsx
@@ -41,7 +41,7 @@ export default function Nav() {
                         </Link>
                     )
                 })}
-                <ThemeSwitcher />
+                {/* <ThemeSwitcher /> */}
             </nav>
         </div>
     )
